274c2872e5bf4fb55b3382c29abdbacc03bd8702
rajaarav257 <raj.aarav257@gmail.com>
2020-04-15 12:03:41 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Validate from "./login";
+
+
+function App() {
+  return (
+
+        < Validate />
+
+  );
+}
+
+export default App;
---
+++ src/login.js
@@ -0,0 +1,138 @@
+import React, { Component } from "react";
+import "./App.css";
+
+// const emailRegex = RegExp(
+//     /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+// );
+const initialState = {
+    name: "",
+    email: "",
+    password: "",
+    nameError: "",
+    emailError: "",
+    passwordError: ""
+};
+
+
+
+
+
+class Validate extends Component {
+    state = initialState;
+    handleChange = event => {
+        const isCheckbox = event.target.type === "checkbox";
+        this.setState({
+            [event.target.password]: isCheckbox
+                ? event.target.checked
+                : event.target.value
+        });
+
+    };
+
+    validates = () => {
+        let nameError = "";
+        let emailError = "";
+        let passwordError = "";
+
+        // let passwordError = "";
+
+        // if (!this.state.name) {
+        //     nameError = "Name cannot be blank";
+        // }
+        if (!this.state.email) {
+            emailError = "Email cannot be blank";
+        }
+        if (!this.state.password) {
+            passwordError = "Password cannot be blank";
+        }
+
+        // if (!this.state.email.includes("@")) {
+        //     emailError = "invalid email";
+        // }
+
+        if (emailError || nameError || passwordError) {
+            this.setState({ passwordError,emailError, nameError });
+            return false;
+        }
+
+        return true;
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        const isValid = this.validates();
+        if (isValid) {
+            console.log(this.state);
+            // clear form
+            this.setState(initialState);
+        }
+    };
+
+    render() {
+
+
+        return (
+            <div className="wrapper">
+                <div className="form-wrapper">
+                    <h1>Login</h1>
+                    <form onSubmit={this.handleSubmit} noValidate>
+                        {/*<div className="email">*/}
+                        {/*    <label htmlFor="name">Name</label>*/}
+                        {/*    <input*/}
+                        {/*        name="name"*/}
+                        {/*        placeholder="email"*/}
+                        {/*        value={this.state.email}*/}
+                        {/*        onChange={this.handleChange}*/}
+
+                        {/*    />*/}
+                        {/*    {this.state.nameError ? (*/}
+                        {/*        <div style={{ fontSize: 12, color: "red" }}>*/}
+                        {/*            {this.state.nameError}*/}
+
+                        {/*        </div>*/}
+                        {/*    ):null}*/}
+                        {/*</div>*/}
+                        <div className="email">
+                            <label htmlFor="email">Email</label>
+                            <input
+                                name="email"
+                                placeholder="email"
+                                value={this.state.email}
+                                onChange={this.handleChange}
+
+                            />
+                            {this.state.emailError ? (
+                            <div style={{ fontSize: 12, color: "red" }}>
+                                {this.state.emailError}
+
+                            </div>
+                            ):null}
+                        </div>
+                        <div className="password">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="password"
+                                value={this.state.password}
+                                onChange={this.handleChange}
+
+                            />
+                            {this.state.passwordError ? (
+                            <div style={{ fontSize: 12, color: "red" }}>
+                                {this.state.passwordError}
+                            </div>
+                            ):null}
+                        </div>
+                        <div className="createAccount">
+                            <button type="submit">Login</button>
+                            <small>Don't Have an Account? <span className="signup-link"> Create one</span></small>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Validate;
